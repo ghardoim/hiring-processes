1e901e79ac211cf19b81f99696c41abed7379c87
Gabriel Hardoim <ghardoim@hotmail.com>
2025-03-17 21:19:30 -0300

    styling with bootstrap

--- app/models/hiring_process.rb
+++ app/models/hiring_process.rb
@@ -1,2 +1,10 @@
 class HiringProcess < ApplicationRecord
+    validates :company, :position, :recruiter, :status, presence: true
+
+    before_save do
+        self.company = company.upcase
+        self.position = position.capitalize
+        self.recruiter = recruiter.titleize
+        self.status = status.downcase
+    end
 end
--- dockerfile
+++ dockerfile
@@ -5,4 +5,10 @@ COPY . /app/
 
 EXPOSE 3000
 
+# RUN ["bundle", "add", "cssbundling-rails"]
+# RUN ["rails", "css:install:bootstrap"]
+
 RUN ["bundle", "install", "--gemfile", "Gemfile"]
+RUN ["rails", "db:migrate"]
+
+CMD ["rails", "s", "-b", "0.0.0.0"]
